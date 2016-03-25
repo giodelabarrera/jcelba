0806c44e4ec274283ed353c844a8f15764a48c16
Giorgio de la Barrera <giodelabarrera@gmail.com>
2016-03-25 01:04:54 +0100

    - Cambio de logo - Texto biblico en el footer - Cambio de imagen final

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,31 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $images = array(
+            '_MG_5585.jpg',
+            '_MG_5618.jpg',
+            '_MG_5613.jpg',
+            '_MG_5645.jpg',
+            '_MG_5652.jpg',
+            '_MG_5615.jpg',
+            '_MG_5655.jpg',
+            '_MG_5679.jpg',
+            '_MG_5681.jpg',
+            '_MG_5684.jpg',
+            '_MG_5694.jpg',
+            '_MG_5712.jpg',
+            '_MG_5731.jpg',
+            '_MG_5740.jpg',
+            '_MG_5748.jpg',
+            '_MG_5753.jpg',
+            '_MG_5775.jpg',
+            '_MG_5780.jpg',            
+            '_MG_5808.jpg',
+            '_MG_5810.jpg',
+        );
+
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'images' => $images,
         ));
     }
 
@@ -24,8 +46,31 @@ class DefaultController extends Controller
      */
     public function galleryAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $images = array(
+            '_MG_5585.jpg',
+            '_MG_5618.jpg',
+            '_MG_5613.jpg',
+            '_MG_5645.jpg',
+            '_MG_5652.jpg',
+            '_MG_5615.jpg',
+            '_MG_5655.jpg',
+            '_MG_5679.jpg',
+            '_MG_5681.jpg',
+            '_MG_5684.jpg',
+            '_MG_5694.jpg',
+            '_MG_5712.jpg',
+            '_MG_5731.jpg',
+            '_MG_5740.jpg',
+            '_MG_5748.jpg',
+            '_MG_5753.jpg',
+            '_MG_5775.jpg',
+            '_MG_5780.jpg',            
+            '_MG_5808.jpg',
+            '_MG_5810.jpg',
+        );
+
         return $this->render('default/gallery.html.twig', array(
+            'images' => $images,
         ));
     }
 }
